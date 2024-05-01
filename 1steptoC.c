38ec5d815d18e94f99132a8e2089d459adc48d9e
adkhd <138499735+adkhd@users.noreply.github.com>
2024-05-01 14:27:36 +0000

    end

---
+++ 1steptoC.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+/*
+    int one, two, three;
+    printf("숫자를 입력하세요:");
+    scanf("%d %d %d", &one, &two, &three);
+    printf("\n 입략한 숫자: %d %d %d", one, two, three);
+
+
+char c[10]="abcd";
+printf("%s", c);
+*/
+float a, b, c;
+scanf(" %f %f %f", &a, &b, &c);
+printf("\n %f*%f*%f : %f", a, b, c, a*b*c);
+
+char name, fname;
+
+return 0;
+}
